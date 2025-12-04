7ab3b8da4b222fb3e0004b93a03783a5135566cd
Jonas Bork <jonas10bork@gmail.com>
2025-12-04 22:03:50 +0100

    Implemented part2

--- day-3/src/bin/part2.rs
+++ day-3/src/bin/part2.rs
@@ -2,25 +2,31 @@
 
 fn main() {
     let file = include_str!("../data/part1-input.txt");
-    let result = part1(file);
+    let result = part2(file);
     println!("{}", result);
 }
 
-fn part1(input: &str) -> i64 {
-    input.lines().map(joltage).sum()
+fn part2(input: &str) -> i64 {
+    input.lines().map(|line| joltage(line, 12)).sum()
 }
 
-fn joltage(line: &str) -> i64 {
-    combinations(line)
-        .map(|s| s.parse::<i64>().unwrap())
-        .max()
-        .unwrap()
+fn joltage(line: &str, length: usize) -> i64 {
+    joltage_str(line, length).parse::<i64>().unwrap()
 }
 
-fn combinations<'a>(line: &'a str) -> impl Iterator<Item = &str> {
-    line.chars()
-        .enumerate()
-        .flat_map(|(i, x)| combinations(&'a line[i + 1..]).map(|y| format!("{x}{y}").as_str()))
+fn joltage_str(line: &str, length: usize) -> String {
+    if length == 0 {
+        return "".to_owned();
+    }
+
+    let nums: Vec<_> = line[0..line.len() - length + 1]
+        .chars()
+        .map(|c| c.to_string().parse::<i64>().unwrap())
+        .collect();
+    let max = nums.iter().max().unwrap();
+    let (i, _) = nums.iter().enumerate().find(|(_, x)| *x == max).unwrap();
+
+    line[i..i + 1].to_string() + joltage_str(&line[i + 1..], length - 1).as_str()
 }
 
 #[cfg(test)]
@@ -33,14 +39,14 @@ mod tests {
 811111111111119
 234234234234278
 818181911112111";
-        let result = part1(input);
-        assert_eq!(result, 357);
+        let result = part2(input);
+        assert_eq!(result, 3121910778619);
     }
 
     #[test]
-    fn test_1() {
+    fn test_single() {
         let input = "987654321111111";
-        let result = part1(input);
-        assert_eq!(result, 98);
+        let result = part2(input);
+        assert_eq!(result, 987654321111);
     }
 }
